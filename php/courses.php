bb29bf97e8efd1fe6bdc992806c28c210e7aec07
viroj fernando <fernandoviroj@gmail.com>
2022-05-22 16:13:30 +0530

    php

--- php/courses.php
+++ php/courses.php
@@ -1,9 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header("Access-Control-Max-Age", "3600");
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Allow-Credentials", "true");
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
